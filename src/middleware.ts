fa4579f8f0b4c9af336857303b7e6f20ad77aa94
Liam Murray <liam@murray.to>
2023-12-11 06:53:06 -0600

    Remove middleware console logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,14 @@ import { has, get } from "@vercel/edge-config";
 import { GuideRecord, RedirectRecord } from "@/types/edge";
 
 export async function middleware(req: NextRequest) {
-	console.log("middleware called");
 	const path = req.nextUrl.pathname;
 	const redirects = (await get("redirects")) as RedirectRecord;
 	const guides = (await get("guides")) as GuideRecord;
-	console.log(Object.keys(redirects));
-	console.log(path);
 
 	if (Object.keys(redirects).includes(path)) {
-		console.log("doing redirect");
 		return NextResponse.redirect(new URL(redirects[path]));
 	}
 	if (path.startsWith("/guide/") && Object.keys(guides).includes(path.replace("/guide/", ""))) {
-		console.log("doing guide");
 		return NextResponse.redirect(new URL(guides[path].url));
 	}
 
